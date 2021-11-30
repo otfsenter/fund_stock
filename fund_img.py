560d817641a2f92cd247be5f3f355f133434b2dc
otfsenter <newellzhou@163.com>
2021-11-30 23:12:07 +0800

    format code

--- fund_img.py
+++ fund_img.py
@@ -2,7 +2,9 @@ import dataframe_image as dfi
 import pandas as pd
 import requests
 
-ji_code = [
+url_fund = 'http://hq.sinajs.cn/list=f_%s'
+
+fund_codes = [
     '040046',
     '000834',
     '161130',
@@ -10,20 +12,10 @@ ji_code = [
     '161725',
     '502056',
     '398051',
+    '260108',
 ]
 
 head = ['名字', '代码', '单位净值', '累计净值', '昨日净值', '跌/涨', '日期', '份额规模']
-
-data_list = []
-for i in ji_code:
-    jijin_jin = requests.get('http://hq.sinajs.cn/list=f_' + i)
-    a = jijin_jin.text
-    name, today_nav, history_nav, yesterday_nav, date, scale = a.split('"')[1].split(',')
-    rate = str(round((float(today_nav) - float(yesterday_nav)) / float(yesterday_nav) * 100, 2)) + '%'
-    scale = str(round(float(scale), 2)) + '亿'
-    data_list.append([str(i) for i in [name, i, today_nav, history_nav, yesterday_nav, rate, date, scale]])
-
-df = pd.DataFrame(data=data_list, columns=head)
 
 
 def style_negative(v):
@@ -36,6 +28,19 @@ def style_negative(v):
         return None
 
 
-s = df.style.applymap(style_negative, subset="跌/涨")
+def main():
+    data_list = []
+    for i in fund_codes:
+        response = requests.get(url_fund % i).text
+        name, today_nav, history_nav, yesterday_nav, date, scale = response.split('"')[1].split(',')
+        rate = str(round((float(today_nav) - float(yesterday_nav)) / float(yesterday_nav) * 100, 2)) + '%'
+        scale = str(round(float(scale), 2)) + '亿'
+        data_list.append([str(i) for i in [name, i, today_nav, history_nav, yesterday_nav, rate, date, scale]])
 
-dfi.export(s, "mytable.png")
+    df = pd.DataFrame(data=data_list, columns=head)
+    s = df.style.applymap(style_negative, subset="跌/涨")
+    dfi.export(s, "mytable.png")
+
+
+if __name__ == '__main__':
+    main()
--- fund_text_cmd.py
+++ fund_text_cmd.py
@@ -3,6 +3,19 @@ import requests
 from colorama import Fore
 from tabulate import tabulate
 
+fund_codes = [
+    '040046',
+    '000834',
+    '161130',
+    '161128',
+    '161725',
+    '502056',
+    '398051',
+    '260108',
+]
+
+head = ['名字', '代码', '单位净值', '累计净值', '昨日净值', '跌/涨', '日期', '份额规模']
+
 
 class Colored:
     # 前景色:红色  背景色:默认
@@ -23,32 +36,23 @@ class Colored:
         return Fore.LIGHTBLUE_EX + s + Fore.RESET
 
 
-color = Colored()
+def main():
+    color = Colored()
 
-ji_code = [
-    '040046',
-    '000834',
-    '161130',
-    '161128',
-    '161725',
-    '502056',
-    '398051',
-]
+    data_list = []
+    for i in fund_codes:
+        response = requests.get('http://hq.sinajs.cn/list=f_' + i).text
+        name, today_nav, history_nav, yesterday_nav, date, scale = response.split('"')[1].split(',')
+        rate = str(round((float(today_nav) - float(yesterday_nav)) / float(yesterday_nav) * 100, 2)) + '%'
+        if rate.startswith('-'):
+            rate = color.green(rate)
+        else:
+            rate = color.red(rate)
+        scale = str(round(float(scale), 2)) + '亿'
+        data_list.append([str(i) for i in [name, i, today_nav, history_nav, yesterday_nav, rate, date, scale]])
 
-head = ['名字', '代码', '单位净值', '累计净值', '昨日净值', '跌/涨', '日期', '份额规模']
+    print(tabulate(data_list, head, "grid", stralign='center'))
 
-data_list = []
-for i in ji_code:
-    jijin_jin = requests.get('http://hq.sinajs.cn/list=f_' + i)
-    a = jijin_jin.text
-    name, today_nav, history_nav, yesterday_nav, date, scale = a.split('"')[1].split(',')
-    rate = str(round((float(today_nav) - float(yesterday_nav)) / float(yesterday_nav) * 100, 2)) + '%'
-    if rate.startswith('-'):
-        rate = color.green(rate)
-    else:
-        rate = color.red(rate)
-    scale = str(round(float(scale), 2)) + '亿'
 
-    data_list.append([str(i) for i in [name, i, today_nav, history_nav, yesterday_nav, rate, date, scale]])
-
-print(tabulate(data_list, head, "grid", stralign='center'))
+if __name__ == '__main__':
+    main()
